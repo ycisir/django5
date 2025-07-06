cfa140d7c118e4e8ef4ea1dc9095a341b81ea5cc
yasir <mohammadyasirkmt@gmail.com>
2025-07-06 20:19:27 +0530

    hyper link using url tag, template inside template using include tag and with only keyword

--- 01_basics/course/urls.py
+++ 01_basics/course/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from course import views
 
 urlpatterns = [
-    path('django/', views.learn_django, name='learn_django'),
-    path('python/', views.learn_python, name='learn_python'),
+    path('django/', views.django, name='django'),
+    path('python/', views.python, name='python'),
 ]
--- 01_basics/course/views.py
+++ 01_basics/course/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 
-def learn_python(request):
+def python(request):
 	name = 'python'
 	description = 'Python is a high-level, general-purpose programming language. Its design philosophy emphasizes code readability with the use of significant indentation.'
 	context = {
 		'name': name,
 		'description':description,
+		'courses': 'fastapi'
 	}
 	return render(request, 'course/python.html', context)
 
 
-def learn_django(request):
+def django(request):
 	name = 'django'
 	description = 'Django is a free and open-source, Python-based web framework that facilitates rapid development of secure and scalable web applications. It adheres to the model-template-views (MTV) architectural pattern, which is a variation of the more common model-view-controller (MVC) pattern.'
 	context = {
