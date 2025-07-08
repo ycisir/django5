bfa142d8f7a92c82da6865b6c4247032ba49cf6f
yasir <mohammadyasirkmt@gmail.com>
2025-07-08 16:21:22 +0530

    create superuser, display data base table in django admin panel

--- 02_models/student/models.py
+++ 02_models/student/models.py
@@ -4,3 +4,6 @@ class Profile(models.Model):
 	name = models.CharField(max_length=50)
 	email = models.EmailField(max_length=50)
 	city = models.CharField(max_length=50)
+
+	def __str__(self):
+		return str(self.name)
