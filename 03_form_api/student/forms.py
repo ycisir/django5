636cfe919fccac5b74852e6c2b4a9d7d2cc3bdc0
yasir <mohammadyasirkmt@gmail.com>
2025-07-08 21:52:52 +0530

    Django Form API Create Django Form

---
+++ 03_form_api/student/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+class Registration(forms.Form):
+	first_name = forms.CharField()
+	last_name = forms.CharField()
+	email = forms.EmailField()
+	city = forms.CharField()
+
+
+class Login(forms.Form):
+	email = forms.EmailField()
+	password = forms.CharField()
---
+++ 03_form_api/student/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from student.forms import Registration, Login
+
+def registration(request):
+	form = Registration()
+	context = {
+		'form': form,
+	}
+	return render(request, 'student/registration.html', context)
+
+
+def login(request):
+	form = Login()
+	# form = Login(auto_id='login_%s')
+	# form = Login(auto_id=True)	# remove id_
+	# form = Login(auto_id=False) # without label tag
+	# field_order, label_suffix, initial={'name':'somevalue'}
+	context = {
+		'form': form,
+	}
+	return render(request, 'student/login.html', context)
