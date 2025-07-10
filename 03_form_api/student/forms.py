115df5387aa0788a34e4d74e64d3cb31a81b5497
yasir <mohammadyasirkmt@gmail.com>
2025-07-10 22:55:08 +0530

    Django Form Built-in Validator and Custom Validators

--- 03_form_api/student/forms.py
+++ 03_form_api/student/forms.py
@@ -1,8 +1,14 @@
 from django import forms
+from django.core import validators
+
+# custom validators
+def gmail_only(value):
+	if not value.split('@')[1] == 'gmail.com':
+		raise forms.ValidationError('gmail domain needed!')
 
 class Register(forms.Form):
-	name = forms.CharField()
-	email = forms.EmailField()
+	name = forms.CharField(validators=[validators.MaxLengthValidator(10), validators.MinLengthValidator(4)])
+	email = forms.EmailField(validators=[gmail_only])
 	password = forms.CharField(widget=forms.PasswordInput())
 
 
@@ -20,15 +26,15 @@ class Register(forms.Form):
 
 
 	# validate all at once
-	def clean(self):
-		cleaned_data = super().clean()
-		name_value = self.cleaned_data.get('name')
-		email_value = self.cleaned_data.get('email')
+	# def clean(self):
+	# 	cleaned_data = super().clean()
+	# 	name_value = self.cleaned_data.get('name')
+	# 	email_value = self.cleaned_data.get('email')
 
-		if name_value and len(name_value) < 5:
-			self.add_error('name', 'Should be more than 5 characters!')
+	# 	if name_value and len(name_value) < 5:
+	# 		self.add_error('name', 'Should be more than 5 characters!')
 
-		if email_value and email_value.split('@')[1] != 'gmail.com':
-			self.add_error('email', 'Gmail domain needed!')
+	# 	if email_value and email_value.split('@')[1] != 'gmail.com':
+	# 		self.add_error('email', 'Gmail domain needed!')
 
-		return cleaned_data
+	# 	return cleaned_data
